d952134af6f1b53bfcc811c7b9496b54997e1d5b
Zhaoyun07 <158839394+Zhaoyun07@users.noreply.github.com>
2024-02-18 20:32:28 +0700

    Update gasbang.py

--- gasbang.py
+++ gasbang.py
@@ -292,7 +292,7 @@ def passwrd():
 		for yuzong in id2:
 			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
 			frs = nmf.split(' ')[0]
-			pwv = ['kamu nanya','kamunanya','kata sandi']
+			pwv = ['sayang','sayangku','cintaku']
 			if len(nmf)<6:
 				if len(frs)<3:
 					pass
